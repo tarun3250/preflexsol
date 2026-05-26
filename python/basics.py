9a1d9d810ac5fd6a6938ce9959d172aed658cd2e
tarun3250 <tarun03250@gmail.com>
2026-05-26 10:58:32 +0530

    python basics

---
+++ python/basics.py
@@ -0,0 +1,17 @@
+#variables
+
+name='tarun'
+age=23
+college='ms ramaiah'
+
+print(name,' age is',age)
+print(type(age))
+
+#input and output
+skill=print(input('enter favourite skill'))
+stu_age=input('enter age of student:')
+print(type(stu_age))
+
+#type conversion
+stu_aget=int(input('enter age of students'))
+print(type(stu_aget))
